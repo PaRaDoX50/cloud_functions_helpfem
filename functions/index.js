1ee5cbca013cd94aea2d72520cc490257e082c80
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-11-08 03:23:33 +0530

    File structure changed

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,9 @@ admin.initializeApp(functions.config().firebase);
 
 exports.sendDangerAlert = functions.https.onCall((data, context) => {
   uid= data['uid']
+  documentSnapshot.ref.collection("users").update({ 
+    isSafe: false
+  })
   const users = admin.firestore().collection('users');
   const payload = {
     notification: {
@@ -23,7 +26,54 @@ exports.sendDangerAlert = functions.https.onCall((data, context) => {
   }).then(documentSnapshots => {
     let alertedUserList = []
     for (let documentSnapshot of documentSnapshots) {
-      if (documentSnapshot.exists && documentSnapshot.get("location") != null && documentSnapshot.get("uid") != uid) {
+      if (documentSnapshot.exists && documentSnapshot.get("location") !== null && documentSnapshot.get("uid") !== uid) {
+        point1 = new GeoPoint(data["latitude"], data["longitude"]);
+        point2 = new GeoPoint(documentSnapshot.get("location")["latitude"], documentSnapshot.get("location")["longitude"]);
+        var distance = point1.distanceTo(point2, true)
+        if (distance < 10) {
+          admin.messaging().sendToDevice(documentSnapshot.get("fcmToken"), payload);
+          alertedUserList.push(documentSnapshot.get("uid"))
+        }
+      } else {
+        console.log(`Found missing document: ${documentSnapshot.id}`);
+      }
+    }
+    console.log("alertedUserList", alertedUserList)
+    documentSnapshot.ref.collection("alerts").add({ 
+      location: { 
+        latitude: data["latitude"], 
+        longitude: data["longitude"] 
+      }, 
+      alertedUserList: alertedUserList,
+      alertTime: Date.now(),
+      uid: uid
+    })
+  });
+});
+
+exports.sendSafeAlert = functions.https.onCall((data, context) => {
+  uid = data['uid']
+  documentSnapshot.ref.collection("users").update({ 
+    isSafe: false
+  })
+  const users = admin.firestore().collection('users');
+  const payload = {
+    notification: {
+      title: 'The person is now safe',
+      body: `Go home now`,
+      icon: 'your-icon-url',
+      click_action: 'FLUTTER_NOTIFICATION_CLICK'
+    },
+    data: {
+      latitude: String(data["latitude"]), longitude: String(data["longitude"]), time: String(Date.now())
+    }
+  };
+  users.listDocuments().then(documentRefs => {
+    return admin.firestore().getAll(...documentRefs);
+  }).then(documentSnapshots => {
+    let alertedUserList = []
+    for (let documentSnapshot of documentSnapshots) {
+      if (documentSnapshot.exists && documentSnapshot.get("location") !== null && documentSnapshot.get("uid") !== uid) {
         point1 = new GeoPoint(data["latitude"], data["longitude"]);
         point2 = new GeoPoint(documentSnapshot.get("location")["latitude"], documentSnapshot.get("location")["longitude"]);
         var distance = point1.distanceTo(point2, true)
