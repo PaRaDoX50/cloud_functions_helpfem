e8e66f9a48039fe0eefadb10e9b4dcae9f238050
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-11-08 12:08:38 +0530

    Commiting latest code for danger and safe alerts

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,8 @@ admin.initializeApp(functions.config().firebase);
 
 exports.sendDangerAlert = functions.https.onCall((data, context) => {
   uid= data['uid']
-  documentSnapshot.ref.collection("users").update({ 
+  // console.log("uid: ", uid)
+  admin.firestore().collection("users").doc(uid).update({ 
     isSafe: false
   })
   const users = admin.firestore().collection('users');
@@ -26,7 +27,8 @@ exports.sendDangerAlert = functions.https.onCall((data, context) => {
   }).then(documentSnapshots => {
     let alertedUserList = []
     for (let documentSnapshot of documentSnapshots) {
-      if (documentSnapshot.exists && documentSnapshot.get("location") !== null && documentSnapshot.get("uid") !== uid) {
+      if (documentSnapshot.exists && documentSnapshot.get("location") && documentSnapshot.get("uid") !== uid) {
+        // console.log("location: ", documentSnapshot.exists && documentSnapshot.get("location"))
         point1 = new GeoPoint(data["latitude"], data["longitude"]);
         point2 = new GeoPoint(documentSnapshot.get("location")["latitude"], documentSnapshot.get("location")["longitude"]);
         var distance = point1.distanceTo(point2, true)
@@ -39,24 +41,24 @@ exports.sendDangerAlert = functions.https.onCall((data, context) => {
       }
     }
     console.log("alertedUserList", alertedUserList)
-    documentSnapshot.ref.collection("alerts").add({ 
+    admin.firestore().collection("alerts").add({ 
       location: { 
         latitude: data["latitude"], 
         longitude: data["longitude"] 
       }, 
       alertedUserList: alertedUserList,
-      alertTime: Date.now(),
+      alertTime: new Date(),
       uid: uid
     })
   });
 });
 
 exports.sendSafeAlert = functions.https.onCall((data, context) => {
+    console.log("starting safealert")
   uid = data['uid']
-  documentSnapshot.ref.collection("users").update({ 
-    isSafe: false
+  admin.firestore().collection("users").doc(uid).update({ 
+    isSafe: true
   })
-  const users = admin.firestore().collection('users');
   const payload = {
     notification: {
       title: 'The person is now safe',
@@ -65,37 +67,27 @@ exports.sendSafeAlert = functions.https.onCall((data, context) => {
       click_action: 'FLUTTER_NOTIFICATION_CLICK'
     },
     data: {
-      latitude: String(data["latitude"]), longitude: String(data["longitude"]), time: String(Date.now())
+      type: "SAFE"
     }
   };
-  users.listDocuments().then(documentRefs => {
-    return admin.firestore().getAll(...documentRefs);
-  }).then(documentSnapshots => {
-    let alertedUserList = []
-    for (let documentSnapshot of documentSnapshots) {
-      if (documentSnapshot.exists && documentSnapshot.get("location") !== null && documentSnapshot.get("uid") !== uid) {
-        point1 = new GeoPoint(data["latitude"], data["longitude"]);
-        point2 = new GeoPoint(documentSnapshot.get("location")["latitude"], documentSnapshot.get("location")["longitude"]);
-        var distance = point1.distanceTo(point2, true)
-        if (distance < 10) {
-          admin.messaging().sendToDevice(documentSnapshot.get("fcmToken"), payload);
-          alertedUserList.push(documentSnapshot.get("uid"))
-        }
-      } else {
-        console.log(`Found missing document: ${documentSnapshot.id}`);
-      }
-    }
-    console.log("alertedUserList", alertedUserList)
-    documentSnapshot.ref.collection("alerts").add({ 
-      location: { 
-        latitude: data["latitude"], 
-        longitude: data["longitude"] 
-      }, 
-      alertedUserList: alertedUserList,
-      alertTime: Date.now(),
-      uid: uid
-    })
-  });
+  console.log("uid: ", uid)
+  admin.firestore().collection('alerts').where("uid", "==", uid).get().then((snapShot) => {
+    if (snapShot.empty) {
+        console.log('No alerts for the user.');
+        return;
+    } 
+    snapShot.forEach((doc) => {
+        console.log("userdata: ", doc.data())
+            console.log("alertedUserList: ", doc.data().alertedUserList)
+            doc.data().alertedUserList && doc.data().alertedUserList.map(user => {
+                console.log("userId: ", user)
+                admin.firestore().collection('users').doc(user).get().then((userAccount) => {
+                    console.log("fmcToken", userAccount.data().fcmToken)
+                    admin.messaging().sendToDevice(userAccount.data().fcmToken, payload);
+                })
+            })
+      })
+  })
 });
 
 // // Create and Deploy Your First Cloud Functions
